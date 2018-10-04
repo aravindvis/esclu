b0c45d746fb87a23d4a9f4de652585a522daf154
Aravind Viswanathan <aravind.viswanathan@imaginea.com>
2018-10-04 07:51:43 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,136 @@
+'use strict';
+
+const fs = require('fs');
+const request =  require('request');
+const program = require('commander');
+const pkg = require('./package.json');
+
+const fullurl = (path ='') => {
+      let url = `http://${program.host}:${program.port}/`;
+      if(program.index){
+         url += program.index + '/';
+        if(program.type){
+           url += program.type + '/';;
+        }  
+      }
+   return url + path.replace(/^\/*/,'');
+};
+
+const handleResponse = (err, res, body) => {
+    if(program.json){
+       console.log(JSON.stringify(err||body));
+    }else{
+       if(err) throw err;
+       console.log(body);
+    }
+};
+
+program
+       .version(pkg.version)
+       .description(pkg.description)
+       .usage('[options] <command> [...]')
+       .option('-o, --host <hostname>', 'hostname [localhost]', 'localhost')
+       .option('-p, --port <number>', 'port number [9200]', '9200')
+       .option('-j, --json', 'format output as JSON')
+       .option('-i, --index <name>', 'which index to use')
+       .option('-t, --type <type>', 'default type for bulk operations')
+       .option('-f, --filter <filter>', 'source filter for query results');
+
+program
+       .command('url [path]')
+       .description('generate the URL for the options and path (default is /)')
+       .action((path = '/') => console.log(fullurl(path)));;
+
+program
+       .command('get [path]')
+       .description('perform an HTTP get request for path (default is /)')
+       .action((path = '/') => {
+            const options = {
+              url: fullurl(path),
+              json: program.json,
+            };
+       
+        request(options, (err, res, body) =>{
+            if(program.json){
+              console.log(JSON.stringify(err || body));
+            }else{
+              if(err) throw err;
+              console.log(body);
+            }
+        });
+      });
+
+program
+       .command('create-index')
+       .description('create an index')
+       .action(() => {
+           if(!program.index){
+              const msg = 'No index specified! Use --index <name>';
+              if(!program.json) throw Error(msg);
+              console.log(JSON.stringify({error:msg}));
+              return;
+       }
+       request.put(fullurl(), handleResponse);
+ });
+
+program
+       .command('list-indices')
+       .alias('li')
+       .description('get a list of indices on this cluster')
+       .action(() => {
+            const path = program.json? '_all':'_cat/indices?v';
+            request({url: fullurl(path), json: program.json},handleResponse);
+       });    
+
+program
+       .command('bulk <file>')
+       .description('read and perform bulk operations from the specified file')
+       .action(file=>{
+              fs.stat(file, (err, stats)=> {
+                 if(err){
+                     if(program.json){
+                         console.log(JSON.stringify(err));
+                         return;
+                     }
+                     throw err;
+                 }
+                 const options = { 
+                            url: fullurl('_bulk'),
+                            json: true,
+                          headers:{
+                               'content-length':stats.size,
+                               'content-type':'application/json',
+                          }
+                 };
+                 const req = request.post(options);
+                 const stream = fs.createReadStream(file);
+                 stream.pipe(req);
+                 req.pipe(process.stdout);
+               });
+         });
+
+program
+       .command('query [queries...]')
+       .alias('q')
+       .description('perform an elasticsearch query')
+       .action((queries=[]) => {
+           const options = {
+               url: fullurl('_search'),
+               json: program.json,
+               qs : {}
+       };
+      
+       if(queries && queries.length){
+          options.qs.q = queries.join(' ');
+       }
+       if(program.filter){
+          options.qs._source = program.filter;
+       }
+       
+       request(options,handleResponse);
+ });
+program.parse(process.argv);
+
+if(!program.args.filter(arg => typeof arg == 'object').length){
+  program.help();
+}
